ed817a4d9c6896baf463bf4657d408433a6a2266
悠悠狮羝LumiCornX <50665778+LumiCorn@users.noreply.github.com>
2024-10-29 15:07:49 +0800

    Update config.ts

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -51,26 +51,11 @@ export default defineConfig4CustomTheme<VdoingThemeConfig>({
           { text: '（建设中）', link: '/pages/9a7ee40fc232253e/' },
         ],
       },
-      {
-        text: '更多',
-        link: '/more/',
+      { text: '更多', link: '/more/', },
+      { text: '关于', link: '/about/' 
         items: [
-          { text: '学习', link: '/pages/f2a556/' },
-          { text: '面试', link: '/pages/aea6571b7a8bae86/' },
-          { text: '心情杂货', link: '/pages/2d615df9a36a98ed/' },
-          { text: '实用技巧', link: '/pages/baaa02/' },
-          { text: '友情链接', link: '/friends/' },
+          { text: "LumiDoc快速介绍', link: '/introduction/', 
         ],
-      },
-      { text: '关于', link: '/about/' },
-      {
-        text: '收藏',
-        link: '/pages/beb6c0bd8a66cea6/',
-        // items: [
-        //   { text: '网站', link: '/pages/beb6c0bd8a66cea6/' },
-        //   { text: '资源', link: '/pages/eee83a9211a70f9d/' },
-        //   { text: 'Vue资源', link: '/pages/12df8ace52d493f6/' },
-        // ],
       },
       {
         text: '索引',
