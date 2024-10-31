c80e93df7de96b270f03fd7341fd277b0ffb3bff
悠悠狮羝LumiCornX <50665778+LumiCorn@users.noreply.github.com>
2024-10-31 23:57:41 +0800

    Update config.ts

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -198,6 +198,7 @@ export default defineConfig4CustomTheme<VdoingThemeConfig>({
 
   document.addEventListener("swup:contentReplaced", renderLatex); // 监听页面切换事件
   document.addEventListener("vuepress:page-update", renderLatex); // 监听页面更新（其实我不知道哪句话是有用的）
+  document.addEventListener("vuepress:pageChanged", renderLatex); // VuePress特有的页面事件
 
     // VuePress内容更新时重新渲染
   if (typeof __VUEPRESS__ !== 'undefined') {
