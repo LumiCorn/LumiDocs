605be88893ee75449d57c1cfc04f31ee401534f1
悠悠狮羝LumiCornX <50665778+LumiCorn@users.noreply.github.com>
2024-10-31 15:41:50 +0800

    更新了404小彩蛋

--- vdoing/layouts/404.vue
+++ vdoing/layouts/404.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="theme-container">
     <div class="theme-vdoing-content">
-      <span>404</span>
+      <span>404闹他放的</span>
       <blockquote>{{ getMsg() }}</blockquote>
       <router-link to="/">返回首页</router-link>
     </div>
@@ -13,7 +13,16 @@ const msgs = [
   `这里什么都没有。`,
   `我是谁？我在哪？`,
   `这是一个Four-Oh-Four.`,
-  `看来我们的链接坏掉了~`
+  `看来我们的链接坏掉了~`,
+  `缘分让我们消失页面之外～ψ(｀∇´)ψ`,
+  `页面被我吃掉啦o(｀ω´ )o桀桀桀`,
+  `404，哦404，哦404，有了你生活美好没烦恼～`,
+  `I'm not a Teapot..`,
+  `你不要木↗️那↘️啊，返回主页啊..！`,
+  `GOD I WISH I COULD SLEEP!`,
+  `I got' I got' I got' I got 404 not found inside my DNA`,
+  `I hope they play fein... But this is in the room404?`,
+  `I could lie, say i like 404, like 404.`
 ]
 
 export default {
